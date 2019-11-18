f11fbc4a21646b93631b2107421c864c0647cf8d
Pedro Bern <pedrobermoreira@gmail.com>
2019-11-18 21:43:15 -0200

    Updates

---
+++ precache-manifest.32917e36f2a3a79845fb1f5afaf732fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d7184ad188972e2d604a99315e0718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df51383c92d464fa3a2f",
+    "url": "/static/css/main.c2ed445e.chunk.css"
+  },
+  {
+    "revision": "e90ac62aa1b45db9ac8e",
+    "url": "/static/js/2.4a3af4d2.chunk.js"
+  },
+  {
+    "revision": "df51383c92d464fa3a2f",
+    "url": "/static/js/main.3e547147.chunk.js"
+  },
+  {
+    "revision": "9f1c1e10b19185048877",
+    "url": "/static/js/runtime-main.c9c94bb1.js"
+  }
+]);
